3950e1ab8f88f9683c92bdd3c639f6487eb59f28
Melanie91 <101412095+Melanie91@users.noreply.github.com>
2022-05-26 14:08:43 +0800

    Update Money.h

--- Money.h
+++ Money.h
@@ -8,20 +8,20 @@ using namespace std;
 class Money
 {
 public:
- Money();
- Money(int);
- //把Time.cpp裡countTimeMoney()算出來的值傳入
- 
- int getTotalMinute();
- int getMoney();
- void setTotalMinute(int);
- void setMoney(int);
+	Money();
+	Money(int);
+	//把Time.cpp裡countTimeMoney()算出來的值傳入
 
- int countMoney(); //算停車費
+	int getTotalMinute();
+	int getMoney();
+	void setTotalMinute(int);
+	void setMoney(int);
+
+	void countMoney(); //算停車費
 
 private:
- int totalminute;
- int money;
+	int totalminute;
+	int money;
 };
 
 #endif
