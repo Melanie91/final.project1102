fa66492ba05bacb6703ec1cf922aa0d0b2a5f375
yuzhen12 <101446183+yuzhen12@users.noreply.github.com>
2022-06-03 10:58:34 +0800

    Update Money.h

--- Money.h
+++ Money.h
@@ -1,5 +1,5 @@
 //Money.h
-//2022.05.29 newest
+//2022.06.03 newest
 
 #ifndef MONEY_H
 #define MONEY_H
@@ -12,17 +12,17 @@ using namespace std;
 class Money
 {
 public:
-	Money();
-	Money(Time);
-	//把Time.cpp裡countTimeMoney()算出來的值傳入
+ Money();
+ Money(Time);
+ //把Time.cpp裡countTimeMoney()算出來的值傳入
 
-	Time getTotalMinute();
+ Time getTotalMinute();
+ void setTotalMinute(Time);
 
-	void setTotalMinute(Time);
-	void countMoney(int); //算停車費
+ void countMoney(int); //算停車費
 
 private:
-	Time totalminute;
+ Time totalminute;
 };
 
 #endif
